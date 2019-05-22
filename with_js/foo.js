df56c9c0dfde2655d96d9db66e56f5e275a28b26
ovoNiku <2891247632@qq.com>
2019-05-23 02:27:17 +0800

    add readme

--- with_js/foo.js
+++ with_js/foo.js
@@ -1,13 +1,13 @@
 var randomColor = function(){
-	var s = ''
-	for (var i = 0; i < 3; i ++) {
-			n = Math.floor(Math.random()*256)
-			console.log(n);
-			s += n
-			s += ','
-	}
-	s=s.substring(0,s.length-1)
-	return 'rgb(' + s + ')'
+		var s = ''
+		for (var i = 0; i < 3; i ++) {
+				n = Math.floor(Math.random()*256)
+				console.log(n);
+				s += n
+				s += ','
+			}
+			s=s.substring(0,s.length-1)
+			return 'rgb(' + s + ')'
 }
 
 $(".color").on("mouseover","li",function(event){
